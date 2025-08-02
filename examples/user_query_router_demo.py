8009828cc5338a5e9937258f335b5b35595eca57
songtao lin <linsongtaovx@163.com>
2025-08-02 17:25:13 +0800

    update UserQueryRouter demo file

--- examples/user_query_router_demo.py
+++ examples/user_query_router_demo.py
@@ -4,10 +4,11 @@ from evoagentx.app.components.user_query_router import UserQueryRouter
 
 
 test_queries = {
-    "clear_enough_queries": [
+    "normal_queries": [
         "I want to add a new node at the end of the workflow to print all middle variables in the workflow",
         "I want to add a button to the frontend to show the current time",
-        "I want to modify the last two workflow's prompt, give more details about the context to make the workflow work better",
+        "I want to modify the prompt of the agents in the last two workflow's nodes, give more details about the context to make the workflow work better",
+        "I want to add a new node at the end of the workflow to print all middle variables in the workflow and make the Data Collection Node more performant",
     ],
     "unclear_queries": [
         "I want to add a new node at the end of the workflow",
@@ -18,7 +19,7 @@ test_queries = {
 if __name__ == "__main__":
     user_query_router = UserQueryRouter()
     results = user_query_router.route_query(
-        user_query=test_queries["clear_enough_queries"][0],
+        user_query=test_queries["normal_queries"][3],
         workflow_context={}
     )
     print(results)
